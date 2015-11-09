92beffc30c5b5fc9634370e1412b15a2f1cc94c1
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-09 13:57:37 +0800

    actors controller updated

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,37 @@
 class ActorsController < ApplicationController
+  before_action :set_actor
+
+  def show
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  def update
+    @actor  = actor.find(params[:id])
+    if @actor.update(actor_params)
+      redirect_to
+    else
+      render :edit
+  end
+
+  private
+  def set_actor
+    @actor = Actor.new(actor_params)
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+  end
 
 end
