67ef40e9952859a9e8caf062fd2e03391f12cc90
AA <andrewhkg@hotmail.com>
2015-11-09 14:08:43 +0800

    student2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,45 @@
 class MoviesController < ApplicationController
 
+  def index
+  @movie = Movie.all
+ end
+
+ def show
+  @movie = Movie.find(params[:id])
+ end
+
+ def new
+  @movie = Movie.new
+ end
+
+ def create
+  @movie = Movie.new(movie_params)
+
+  if @movie.save
+    redirect_to @movie
+  else
+    render :new
+  end
+ end
+
+ def edit
+  @movie = Movie.find(params[:id])
+ end
+
+ def update
+  @movie = Movie.find(arams[:id])
+  if @movie.update(movie_params)
+    redirect_to @movie
+  else
+    render :edit
+  end
+ end
+
+ private
+
+ def movie_params
+  params.require(:movie).permit(:name, :year_of_birth, :thumbnail)
+end
+
+
 end
