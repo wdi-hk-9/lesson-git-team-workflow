d6a6013336e66e439f8f0d8559ab11fe5bf0ed7b
adaau <ada.au.cheng@gmail.com>
2015-11-09 12:52:52 +0800

    added relationships to Review model; added relationship to User model; added full_name method to User model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
-class Review < ActiveRecord::
-  belongs_to :movie
+class Review < ActiveRecord::Base
+  belongs_to :movies
+  belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :reviews
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
