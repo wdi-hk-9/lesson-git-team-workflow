5b65c65c941d30cbbc42c700fa45fa3486c8ee3d
Toni Anne Romero <tonianne.romero@gmail.com>
2015-11-09 12:50:20 +0800

    student 1.  movie and actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,8 @@
 class Actor < ActiveRecord::Base
+  has_and_belong_to :movies
+
+  def age
+    self.year_of_birth - Time.now.year
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ActiveRecord::Base
+  has_and_belong_to :actors
+  has_many :reviews
 end
